0819cfacca3d961d8820df6925f96714d4b6e7c8
nnoya <nnoya@fing.edu.uy>
2025-02-25 16:35:17 +0100

    weights and how to get them in C++ format

--- modelMNIST.py
+++ modelMNIST.py
@@ -1,3 +1,7 @@
+#############################################
+# Train a simple model on the MNIST dataset #
+#############################################
+
 #%%
 import numpy as np
 import tensorflow as tf
@@ -34,3 +38,41 @@ model.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy', metri
 # Train the model
 model.fit(x_train, y_train, epochs=1, batch_size=600)
 # %%
+#########################################
+# Create the weights file in C++ format #
+#########################################
+
+#%%
+# print the weights
+weights = model.get_weights()
+print(f"Number of weights tensors: {len(weights[])}")
+
+#%%
+weight_dict = {}
+for i, w in enumerate(weights):
+    typ = "bias" if i % 2 else "dense"
+    print(f"Layer {i} - Shape: {w.shape} - {typ}")
+    weight_dict[f"layer_{typ}_{i}"] = w
+
+
+#%%
+for k, v in weight_dict.items():
+    print(f"{k} - {v.shape}")
+
+
+#%%
+for i, w in enumerate(weights):
+    with open("weights.cpp", "w") as cpp_file:
+        cpp_file.write("#ifndef WEIGHTS_H\n#define WEIGHTS_H\n\n")
+        for layer_name in weight_dict.keys():
+                weight_data = np.array(weight_dict[layer_name])
+                flat_weights = weight_data.flatten()
+
+                # Generate a C++ array
+                cpp_file.write(f"// {layer_name}\n")
+                cpp_file.write(f"const float {layer_name}[] = {{")
+                cpp_file.write(", ".join(map(str, flat_weights)))
+                cpp_file.write("};\n\n")
+
+        cpp_file.write("#endif // WEIGHTS_H\n")
+print("Done")
